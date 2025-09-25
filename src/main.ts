5b93cc15f5299cbe816099b9ea4177b8c9e8d69d
nativedev001 <nativexdeveloper@gmail.com>
2025-09-26 00:02:27 +0500

    Bind server to 0.0.0.0 for EC2 access

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
   console.log(`🚀 Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
